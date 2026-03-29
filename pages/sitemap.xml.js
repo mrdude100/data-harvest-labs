8cd84550155c221d4015b81baa1993b80d785b0a
mrdude100 <89030781+mrdude100@users.noreply.github.com>
2026-03-30 01:22:19 +0530

    Add Sitemap to Google Search Console

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,38 @@
+const baseUrl = 'https://www.dataharvestlabs.com';
+
+const pages = [
+  { path: '/', priority: '1.0', changefreq: 'weekly' },
+  { path: '/about', priority: '0.9', changefreq: 'monthly' },
+  { path: '/contact', priority: '0.9', changefreq: 'monthly' },
+  { path: '/quote', priority: '0.8', changefreq: 'monthly' },
+  { path: '/testimonials', priority: '0.7', changefreq: 'monthly' },
+];
+
+const generateSitemap = () => {
+  const today = new Date().toISOString().split('T')[0];
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages
+  .map(
+    ({ path, priority, changefreq }) => `  <url>
+    <loc>${baseUrl}${path}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+  </url>`,
+  )
+  .join('\n')}
+</urlset>`;
+};
+
+const Sitemap = () => null;
+
+export const getServerSideProps = async ({ res }) => {
+  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate');
+  res.write(generateSitemap());
+  res.end();
+  return { props: {} };
+};
+
+export default Sitemap;
